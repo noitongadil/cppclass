570f57f79daefc7fcab6d2721b108ff81f038989
noitongadil <btrglm@proton.me>
2025-10-13 16:31:38 -0600

    queue implemented + basic tests for both versions

--- homework/src/queues/queue.h
+++ homework/src/queues/queue.h
@@ -1,6 +1,8 @@
 #pragma once
 
-#include <cstddef> /// for size_t
+#include "gtest/gtest_prod.h" // for FRIEND_TEST
+#include <cstddef> // for size_t
+#include <cstdio> // for stderr
 
 namespace cppclass
 {
@@ -8,33 +10,89 @@ template <typename T> class RingQueue
 {
 public:
     /// @brief Default constructor.
-    RingQueue();
+    RingQueue()
+        : m_head(0)
+        , m_tail(1)
+    {
+    }
 
     /// @brief Array constructor.
-    RingQueue(const T *arr, size_t size);
+    RingQueue(const T *arr, size_t size)
+        : m_head(0)
+        , m_tail(size + 1)
+    {
+        for (int i{}; i < size; i++)
+        {
+            *(m_p_arr + i) = *(arr + i);
+        }
+    }
 
     /// @brief Copy constructor.
-    RingQueue(const RingQueue &other);
+    RingQueue(const RingQueue &other)
+        : m_head(other.m_head)
+        , m_tail(other.m_tail)
+    {
+        for (int i{}; i < other.m_tail; i++)
+        {
+            *(m_p_arr + i) = *(other.m_p_arr + i);
+        }
+    }
 
     /// @brief Move constructor.
-    RingQueue(RingQueue &&other);
+    RingQueue(RingQueue &&other)
+        : m_p_arr(other.m_p_arr)
+        , m_head(other.m_head)
+        , m_tail(other.m_tail)
+    {
+        other.m_p_arr = nullptr;
+        other.m_tail = 0;
+        other.m_head = 0;
+    }
 
     /// @brief Destructor.
-    ~RingQueue();
+    ~RingQueue()
+    {
+        delete[] m_p_arr;
+    }
 
     /// @brief Peek at top data.
-    const T &top() const;
+    const T &top() const
+    {
+        return *(m_p_arr + m_head);
+    }
 
     /// @brief Remove and return top.
-    T pop_front();
+    T pop_front()
+    {
+        auto tmp = *(m_p_arr + m_head);
+        m_head = (m_head >= 1023) ? 0 : m_head + 1;
+        return tmp;
+    }
 
     /// @brief Push data to back.
-    bool push_back(const T &data);
+    bool push_back(const T &data)
+    {
+        if (m_head == m_tail)
+        {
+            return false;
+        }
+
+        *(m_p_arr + m_tail - 1) = data;
+        m_tail = (m_tail >= 1023) ? 0 : m_tail + 1;
+
+        return true;
+    }
 
 private:
     T *m_p_arr = new T[1024]{}; ///< Container to hold data.
     size_t m_head; ///< Index to the start of valid data.
     size_t m_tail; ///< Index to the end of valid data.
+
+    FRIEND_TEST(BasicRingQueueTest, DefaultConstructor);
+    FRIEND_TEST(BasicRingQueueTest, ArrayConstructor);
+    FRIEND_TEST(BasicRingQueueTest, CopyConstructor);
+    FRIEND_TEST(BasicRingQueueTest, MoveConstructor);
+    FRIEND_TEST(RingQueueTest, SetUp);
 };
 
 template <typename T> class LinkQueue
@@ -45,36 +103,118 @@ public:
         T data;
         Node *next;
 
-        Node(T data);
+        Node(T data)
+            : data(data)
+            , next(nullptr)
+        {
+        }
     };
 
     /// @brief Default constructor.
-    LinkQueue();
+    LinkQueue()
+        : m_head(nullptr)
+        , m_tail(nullptr)
+        , m_size(0)
+    {
+    }
 
     /// @brief Array constructor.
-    LinkQueue(const T *arr, size_t size);
+    LinkQueue(const T *arr, size_t size)
+        : LinkQueue()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            push_back(*(arr + i));
+        }
+    }
 
     /// @brief Copy constructor.
-    LinkQueue(const LinkQueue &other);
+    LinkQueue(const LinkQueue &other)
+        : LinkQueue()
+    {
+        Node *current = other.m_head;
+        for (int i = 0; i < other.m_size; i++)
+        {
+            push_back(current->data);
+            current = current->next;
+        }
+    }
 
     /// @brief Move constructor.
-    LinkQueue(LinkQueue &&other);
+    LinkQueue(LinkQueue &&other)
+        : m_head(other.m_head)
+        , m_tail(other.m_tail)
+        , m_size(other.m_size)
+    {
+        other.m_head = nullptr;
+        other.m_tail = nullptr;
+        other.m_size = 0;
+    }
 
     // @brief Destructor.
-    ~LinkQueue();
+    ~LinkQueue()
+    {
+        while (m_head != nullptr)
+        {
+            pop_front();
+        }
+    }
 
     /// @brief Peek at top data.
-    const T &top() const;
+    const T &top() const
+    {
+        if (m_head == nullptr)
+        {
+            fprintf(stderr, "queue empty");
+        }
+
+        return m_head->data;
+    }
 
     /// @brief Remove and return top.
-    T pop_front();
+    T pop_front()
+    {
+        if (m_size == 0)
+        {
+            fprintf(stderr, "queue empty");
+        }
+
+        Node *old_head = m_head;
+        T value = old_head->data;
+        m_head = m_head->next;
+        delete old_head;
+        m_size--;
+
+        return value;
+    }
 
     /// @brief Push data to back.
-    bool push_back(const T &data);
+    bool push_back(const T &data)
+    {
+        Node *new_node = new Node(data);
+        if (m_size == 0)
+        {
+            m_head = new_node;
+            m_tail = new_node;
+        }
+        else
+        {
+            m_tail->next = new_node;
+            m_tail = new_node;
+        }
+        m_size++;
+        return true;
+    }
 
 private:
     Node *m_head; ///< Pointer to the start of list.
     Node *m_tail; ///< Pointer to the end of list.
     size_t m_size; ///< Number of nodes in the list.
+
+    FRIEND_TEST(BasicLinkQueueTest, DefaultConstructor);
+    FRIEND_TEST(BasicLinkQueueTest, ArrayConstructor);
+    FRIEND_TEST(BasicLinkQueueTest, CopyConstructor);
+    FRIEND_TEST(BasicLinkQueueTest, MoveConstructor);
+    FRIEND_TEST(LinkQueueTest, SetUp);
 };
 }
--- homework/tests/tests_queue.cpp
+++ homework/tests/tests_queue.cpp
@@ -1,1 +1,256 @@
 #include "queue.h"
+#include "gtest/gtest.h"
+#include "gtest/gtest_prod.h"
+
+namespace cppclass
+{
+class BasicRingQueueTest : public ::testing::Test
+{
+protected:
+    BasicRingQueueTest()
+    {
+    }
+
+    virtual ~BasicRingQueueTest()
+    {
+    }
+
+    virtual void SetUp()
+    {
+    }
+
+    virtual void TearDown()
+    {
+    }
+};
+
+TEST_F(BasicRingQueueTest, DefaultConstructor)
+{
+    RingQueue<int> queue;
+    EXPECT_EQ(queue.m_tail, 1);
+    EXPECT_EQ(queue.m_head, 0);
+}
+
+TEST_F(BasicRingQueueTest, PushBack)
+{
+    RingQueue<int> queue;
+    EXPECT_TRUE(queue.push_back(1));
+    EXPECT_TRUE(queue.push_back(2));
+    EXPECT_TRUE(queue.push_back(3));
+    EXPECT_TRUE(queue.push_back(4));
+}
+
+TEST_F(BasicRingQueueTest, Top)
+{
+    RingQueue<int> queue;
+    EXPECT_TRUE(queue.push_back(1));
+    EXPECT_TRUE(queue.push_back(2));
+    EXPECT_TRUE(queue.push_back(3));
+    EXPECT_TRUE(queue.push_back(4));
+    EXPECT_EQ(queue.top(), 1);
+}
+
+TEST_F(BasicRingQueueTest, PopFront)
+{
+    RingQueue<int> queue;
+    EXPECT_TRUE(queue.push_back(1));
+    EXPECT_EQ(queue.pop_front(), 1);
+    EXPECT_EQ(queue.top(), 0);
+}
+
+TEST_F(BasicRingQueueTest, ArrayConstructor)
+{
+    int arr[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    RingQueue<int> queue(arr, 10);
+    EXPECT_EQ(queue.m_head, 0);
+    EXPECT_EQ(queue.m_tail, 11);
+    EXPECT_EQ(queue.top(), 0);
+}
+
+TEST_F(BasicRingQueueTest, CopyConstructor)
+{
+    int arr[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    RingQueue<int> queue(arr, 10);
+    RingQueue<int> c_queue(queue);
+
+    EXPECT_EQ(c_queue.m_head, 0);
+    EXPECT_EQ(c_queue.m_tail, 11);
+    EXPECT_EQ(c_queue.top(), 0);
+
+    EXPECT_EQ(queue.m_head, 0);
+    EXPECT_EQ(queue.m_tail, 11);
+    EXPECT_EQ(queue.top(), 0);
+}
+
+TEST_F(BasicRingQueueTest, MoveConstructor)
+{
+    int arr[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    RingQueue<int> queue(arr, 10);
+    EXPECT_EQ(queue.m_head, 0);
+    EXPECT_EQ(queue.m_tail, 11);
+    EXPECT_EQ(queue.top(), 0);
+
+    RingQueue<int> c_queue(std::move(queue));
+    EXPECT_EQ(c_queue.m_head, 0);
+    EXPECT_EQ(c_queue.m_tail, 11);
+    EXPECT_EQ(c_queue.top(), 0);
+
+    EXPECT_EQ(queue.m_head, 0);
+    EXPECT_EQ(queue.m_tail, 0);
+}
+
+class RingQueueTest : public BasicRingQueueTest
+{
+protected:
+    RingQueue<int> *p_queue;
+
+    virtual void SetUp()
+    {
+        BasicRingQueueTest::SetUp();
+
+        p_queue = new RingQueue<int>();
+        for (int i = 0; i < 1024; i++)
+        {
+            p_queue->push_back(i);
+        }
+    }
+
+    virtual void TearDown()
+    {
+        BasicRingQueueTest::TearDown();
+
+        delete p_queue;
+    }
+};
+
+TEST_F(RingQueueTest, MassRemoval)
+{
+    for (int i = 0; i < 1024; i++)
+    {
+        EXPECT_EQ(p_queue->top(), p_queue->pop_front());
+    }
+}
+
+class BasicLinkQueueTest : public ::testing::Test
+{
+protected:
+    BasicLinkQueueTest()
+    {
+    }
+
+    virtual ~BasicLinkQueueTest()
+    {
+    }
+
+    virtual void SetUp()
+    {
+    }
+
+    virtual void TearDown()
+    {
+    }
+};
+
+TEST_F(BasicLinkQueueTest, DefaultConstructor)
+{
+    LinkQueue<int> queue;
+    EXPECT_EQ(queue.m_tail, nullptr);
+    EXPECT_EQ(queue.m_head, nullptr);
+}
+
+TEST_F(BasicLinkQueueTest, PushBack)
+{
+    LinkQueue<int> queue;
+    EXPECT_TRUE(queue.push_back(1));
+    EXPECT_TRUE(queue.push_back(2));
+    EXPECT_TRUE(queue.push_back(3));
+    EXPECT_TRUE(queue.push_back(4));
+}
+
+TEST_F(BasicLinkQueueTest, Top)
+{
+    LinkQueue<int> queue;
+    EXPECT_TRUE(queue.push_back(1));
+    EXPECT_TRUE(queue.push_back(2));
+    EXPECT_TRUE(queue.push_back(3));
+    EXPECT_TRUE(queue.push_back(4));
+    EXPECT_EQ(queue.top(), 1);
+}
+
+TEST_F(BasicLinkQueueTest, PopFront)
+{
+    LinkQueue<int> queue;
+    EXPECT_TRUE(queue.push_back(1));
+    EXPECT_EQ(queue.pop_front(), 1);
+}
+
+TEST_F(BasicLinkQueueTest, ArrayConstructor)
+{
+    int arr[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    LinkQueue<int> queue(arr, 10);
+    EXPECT_EQ(queue.m_head->data, 0);
+    EXPECT_EQ(queue.m_tail->data, 9);
+    EXPECT_EQ(queue.top(), 0);
+}
+
+TEST_F(BasicLinkQueueTest, CopyConstructor)
+{
+    int arr[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    LinkQueue<int> queue(arr, 10);
+    LinkQueue<int> c_queue(queue);
+
+    EXPECT_EQ(c_queue.m_head->data, queue.m_head->data);
+    EXPECT_EQ(c_queue.m_tail->data, queue.m_tail->data);
+    EXPECT_EQ(c_queue.top(), 0);
+    EXPECT_EQ(queue.top(), 0);
+}
+
+TEST_F(BasicLinkQueueTest, MoveConstructor)
+{
+    int arr[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    LinkQueue<int> queue(arr, 10);
+    EXPECT_EQ(queue.m_head->data, 0);
+    EXPECT_EQ(queue.m_tail->data, 9);
+    EXPECT_EQ(queue.top(), 0);
+
+    LinkQueue<int> m_queue(std::move(queue));
+    EXPECT_EQ(m_queue.m_head->data, 0);
+    EXPECT_EQ(m_queue.m_tail->data, 9);
+    EXPECT_EQ(m_queue.top(), 0);
+
+    EXPECT_EQ(queue.m_head, nullptr);
+    EXPECT_EQ(queue.m_tail, nullptr);
+}
+
+class LinkQueueTest : public BasicLinkQueueTest
+{
+protected:
+    LinkQueue<int> *p_queue;
+
+    virtual void SetUp()
+    {
+        BasicLinkQueueTest::SetUp();
+
+        p_queue = new LinkQueue<int>();
+        for (int i = 0; i < 1024; i++)
+        {
+            p_queue->push_back(i);
+        }
+    }
+
+    virtual void TearDown()
+    {
+        BasicLinkQueueTest::TearDown();
+
+        delete p_queue;
+    }
+};
+
+TEST_F(LinkQueueTest, MassRemoval)
+{
+    for (int i = 0; i < 1024; i++)
+    {
+        EXPECT_EQ(p_queue->pop_front(), i);
+    }
+}
+}
