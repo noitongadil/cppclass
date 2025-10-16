b816cf263c7f3502a020220301cc79aaaab65b31
noitongadil <btrglm@proton.me>
2025-10-16 12:18:46 -0600

    heap implemented + basic tests

--- homework/src/heaps/heap.h
+++ homework/src/heaps/heap.h
@@ -1,13 +1,20 @@
 #pragma once
 
 #include <vector> // for std::vector
-#include <cstddef> // for size_t
+#include <stdlib.h> // for size_t
 
-namespace cppclass {
-template <typename T> class Heap {
+#include "gtest/gtest.h"
+
+namespace cppclass
+{
+template <typename T> class Heap
+{
 public:
     /// @brief Constructs an empty heap.
-    Heap();
+    Heap()
+    {
+        m_vect.reserve(10);
+    }
 
     /**
      * @brief Constructs a heap from an array.
@@ -15,14 +22,23 @@ public:
      * @param arr Pointer to the array.
      * @param size Size of the array.
      */
-    Heap(const T *arr, size_t size);
+    Heap(const T *arr, size_t size)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            insert(*(arr + i));
+        }
+    }
 
     /**
      * @brief Copy constructs a heap from another heap.
      *
      * @param other Reference to the heap to copy from.
      */
-    Heap(const Heap &other);
+    Heap(const Heap &other)
+        : m_vect(other.m_vect)
+    {
+    }
 
     /**
      * @brief Copy assignment operator.
@@ -30,14 +46,20 @@ public:
      * @param other Reference to the heap to copy from.
      * @return Reference to this heap after assignment.
      */
-    Heap& operator=(const Heap &other);
+    Heap &operator=(const Heap &other)
+    {
+        m_vect = other.m_vect;
+    }
 
     /**
      * @brief Move constructs a heap from another heap.
      *
      * @param other R-Value reference to the heap to move from.
      */
-    Heap(Heap &&other);
+    Heap(Heap &&other)
+        : m_vect(std::move(other.m_vect))
+    {
+    }
 
     /**
      * @brief Move assignment operator.
@@ -45,34 +67,54 @@ public:
      * @param other R-Value reference to the heap to move from.
      * @return Reference to this heap after assignment.
      */
-    Heap& operator=(Heap &&other);
+    Heap &operator=(Heap &&other)
+    {
+        m_vect = std::move(other.m_vect);
+    }
 
     /// @brief Destroys the heap.
-    ~Heap();
+    ~Heap() = default;
 
     /**
      * @brief Peek at the root of the heap.
      *
      * @return The root of the heap.
      */
-    const T& top() const;
+    const T &top() const
+    {
+        return m_vect[0];
+    }
 
     /**
      * @brief Removes and returns the root of the heap.
      *
      * @return The root of the heap.
      */
-    T pop_top();
+    T pop()
+    {
+        T tmp = m_vect[0];
+        std::swap(m_vect[0], m_vect.back());
+        m_vect.pop_back();
+        heapify_down();
+        return tmp;
+    }
 
     /**
      * @brief Inserts @p data to the heap.
      *
      * @param data Data to insert into the heap.
      */
-    void insert(const T &data);
+    void insert(const T &data)
+    {
+        m_vect.emplace_back(data);
+        heapify_up(m_vect.size() - 1);
+    }
 
     /// @brief Returns the number of elements in the heap.
-    size_t size() const;
+    size_t size() const
+    {
+        return m_vect.size();
+    }
 
     /**
      * @brief Equality comparison.
@@ -80,7 +122,10 @@ public:
      * @param other The heap to compare with.
      * @return True if the heaps do not differ in order or value.
      */
-    bool operator==(const Heap &other) const;
+    bool operator==(const Heap &other) const
+    {
+        return m_vect == other.m_vect;
+    }
 
     /**
      * @brief Inequality comparison.
@@ -88,30 +133,129 @@ public:
      * @param other The heap to compare with.
      * @return True if the heaps differ in order or value.
      */
-    bool operator!=(const Heap &other) const;
+    bool operator!=(const Heap &other) const
+    {
+        return !(*this == other);
+    }
 
 private:
-    std::vector<T> m_arr; ///< Container to hold the heap's elements.
+    std::vector<T> m_vect; ///< Container to hold the heap's elements.
 
     /**
      * @brief Checks the validity of the heap.
      *
      * @return True if valid, false if invalid.
      */
-    bool is_valid() const;
+    bool is_valid() const
+    {
+        const size_t SIZE = m_vect.size();
+        for (int i = 0; left_child(i) < SIZE; i++)
+        {
+            if (m_vect[left_child(i)] < m_vect[i] ||
+                (right_child(i) < SIZE && m_vect[right_child(i)] < m_vect[i]))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
 
     /**
      * @brief Restores the heap property by moving the element at @p index up.
      *
      * @param index Index of the element to move up.
      */
-    void heapify_up(size_t index);
+    void heapify_up(size_t child_index)
+    {
+        size_t parent_index = parent(child_index);
+        while (child_index > 0)
+        {
+            if (m_vect[child_index] >= m_vect[parent_index])
+            {
+                return;
+            }
+
+            std::swap(m_vect[child_index], m_vect[parent_index]);
+            child_index = parent_index;
+            parent_index = parent(child_index);
+        }
+    }
 
     /**
-     * @brief Restores the heap property by moving the element at @p index down.
-     *
-     * @param index Index of the element to move down.
+     * @brief Maintains the heap property by moving the root down for deletion.
      */
-    void heapify_down(size_t index);
+    void heapify_down()
+    {
+        size_t parent = 0;
+        size_t l_child;
+        size_t r_child;
+        size_t smallest;
+        const size_t SIZE = m_vect.size();
+
+        while (true)
+        {
+            l_child = left_child(parent);
+            r_child = right_child(parent);
+            smallest = parent;
+
+            if (l_child < SIZE && m_vect[l_child] < m_vect[smallest])
+            {
+                smallest = l_child;
+            }
+
+            if (r_child < SIZE && m_vect[r_child] < m_vect[smallest])
+            {
+                smallest = r_child;
+            }
+
+            if (smallest == parent)
+            {
+                break;
+            }
+
+            std::swap(m_vect[parent], m_vect[smallest]);
+            parent = smallest;
+        }
+    }
+
+    /**
+     * @brief Returns the index of the left child of @p parent.
+     *
+     * @param parent_index Index of the parent.
+     * @return Index of the left child;
+     */
+    inline size_t left_child(size_t parent_index) const
+    {
+        return parent_index * 2 + 1;
+    }
+
+    /**
+     * @brief Returns the index of the right child of @p parent.
+     *
+     * @param parent_index Index of the parent.
+     * @return Index of the right child;
+     */
+    inline size_t right_child(size_t parent_index) const
+    {
+        return parent_index * 2 + 2;
+    }
+
+    /**
+     * @brief Returns the index of the parent of @p child.
+     *
+     * @param child_index Index of the child.
+     * @return Index of the parent;
+     */
+    inline size_t parent(size_t child_index) const
+    {
+        return (child_index - 1) / 2;
+    }
+
+    void print()
+    {
+    }
+
+    FRIEND_TEST(BasicHeapTest, Insert);
+    FRIEND_TEST(BasicHeapTest, Pop);
 };
 } // namespace cppclass
--- homework/tests/tests_heap.cpp
+++ homework/tests/tests_heap.cpp
@@ -1,1 +1,66 @@
 #include "heap.h"
+#include "gtest/gtest.h"
+
+namespace cppclass
+{
+class BasicHeapTest : public ::testing::Test
+{
+protected:
+    BasicHeapTest()
+    {
+    }
+
+    virtual ~BasicHeapTest()
+    {
+    }
+
+    virtual void SetUp()
+    {
+        auto seed_gen = testing::UnitTest::GetInstance();
+        int seed = seed_gen->random_seed();
+        std::srand(seed);
+        printf("seed=%d\n", seed);
+    }
+
+    virtual void TearDown()
+    {
+    }
+};
+
+TEST_F(BasicHeapTest, Insert)
+{
+    Heap<int> heap;
+    heap.insert(5);
+    heap.insert(10);
+    heap.insert(12);
+    heap.insert(11);
+    ASSERT_TRUE(heap.is_valid());
+}
+
+TEST_F(BasicHeapTest, Top)
+{
+    Heap<int> heap;
+    heap.insert(5);
+    heap.insert(10);
+    heap.insert(12);
+    heap.insert(11);
+    EXPECT_EQ(heap.top(), 5);
+}
+
+TEST_F(BasicHeapTest, Pop)
+{
+    Heap<int> heap;
+    for (int i = 0; i < 15; i++)
+    {
+        heap.insert(rand());
+        ASSERT_TRUE(heap.is_valid());
+    }
+
+    for (int i = 0; i < 15; i++)
+    {
+        ASSERT_TRUE(heap.pop() >= 0);
+        ASSERT_TRUE(heap.is_valid());
+    }
+    printf("\n");
+}
+}
