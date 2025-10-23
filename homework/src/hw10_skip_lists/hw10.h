6bc3a7bb3bab16730f864e26b4e56cb6ef79abcd
noitongadil <btrglm@proton.me>
2025-10-23 17:59:14 -0600

    hw10 methods implemented + basic tests

--- homework/src/hw10_skip_lists/hw10.h
+++ homework/src/hw10_skip_lists/hw10.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include <cstddef> // for size_t
+#include <ctime> // for time()
+#include <cstdlib> // for rand()
+#include <cmath> // for log2
+#include "gtest/gtest_prod.h" // for FRIEND_TEST
 
 namespace cppclass
 {
@@ -19,19 +23,35 @@ private:
          * @param data Reference to the data to be contained.
          * @param height The height of the node.
          */
-        Node(const T &data, const size_t height);
+        Node(const T &data, const size_t height)
+            : data(data)
+            , height(height)
+        {
+            levels = new Node *[height];
+            for (int i = 0; i < height; i++)
+            {
+                levels[i] = nullptr;
+            }
+        }
 
         /**
          * @brief Destroys a node.
          */
-        ~Node();
+        ~Node()
+        {
+            delete[] levels;
+        }
     };
 
 public:
     /**
      * @brief Constructs an empty skip list.
      */
-    SkipList();
+    SkipList()
+        : m_size(0)
+    {
+        SkipListSetUp();
+    }
 
     /**
      * @brief Constructs a skip list from an array.
@@ -39,26 +59,59 @@ public:
      * @param arr Pointer to the array.
      * @param size Size of the array.
      */
-    SkipList(const T *arr, const size_t size);
+    SkipList(const T *arr, const size_t size)
+        : m_size(0)
+    {
+        SkipListSetUp();
+        for (int i = 0; i < size; i++)
+        {
+            insert(*(arr + i));
+        }
+    }
 
     /**
      * @brief Copy constructs a skip list from @p other.
      *
      * @param other Reference to the skip list to copy from.
      */
-    SkipList(const SkipList &other);
+    SkipList(const SkipList &other)
+        : m_size(0)
+    {
+        SkipListSetUp();
+        Node *curr = other.m_head->levels[0];
+        for (int i = 0; i < other.m_size; i++)
+        {
+            this->insert(curr->data);
+            curr = curr->levels[0];
+        }
+    }
 
     /**
      * @brief Move contructs a skip list from @p other
      *
      * @param other R-Value reference to the skip list to move from.
      */
-    SkipList(SkipList &&other);
+    SkipList(SkipList &&other)
+        : m_size(other.m_size)
+        , m_head(other.m_head)
+    {
+        other.m_size = 0;
+        other.m_head = nullptr;
+    }
 
     /**
      * @brief Destroys the skip list.
      */
-    ~SkipList();
+    ~SkipList()
+    {
+        Node *curr = m_head;
+        while (curr != nullptr)
+        {
+            Node *next = curr->levels[0];
+            delete curr;
+            curr = next;
+        }
+    }
 
     /**
      * @brief Searches the skip list for the node with @p data.
@@ -66,15 +119,61 @@ public:
      * @param data Data to search for.
      * @return If found pointer to the node with @p data, else nullptr.
      */
-    Node *search(const T &data) const;
+    Node *search(const T &data) const
+    {
+        if (m_head->levels[0] == nullptr)
+        {
+            return nullptr;
+        }
+        Node *curr = m_head;
+
+        for (int lvl = MAX_LEVEL - 1; lvl >= 0; lvl--)
+        {
+            while (curr->levels[lvl] != nullptr &&
+                   curr->levels[lvl]->data < data)
+            {
+                curr = curr->levels[lvl];
+            }
+        }
+
+        if (curr->levels[0] != nullptr && curr->levels[0]->data == data)
+        {
+            return curr->levels[0];
+        }
+
+        return nullptr;
+    }
 
     /**
      * @brief Inserts a node with @p data into the skip list.
      *
      * @param data Data to add to skip list.
-     * @return True if inserted, otherwise false.
+     * @return True if inserted, false if failed.
      */
-    bool insert(const T &data);
+    bool insert(const T &data)
+    {
+        size_t level = log2(rand() % (1 << MAX_LEVEL));
+        Node *new_node = new Node(data, level);
+
+        Node *curr = m_head;
+        for (int lvl = MAX_LEVEL - 1; lvl >= 0; lvl--)
+        {
+            while (curr->levels[lvl] != nullptr &&
+                   curr->levels[lvl]->data < new_node->data)
+            {
+                curr = curr->levels[lvl];
+            }
+
+            if (lvl < new_node->height)
+            {
+                new_node->levels[lvl] = curr->levels[lvl];
+                curr->levels[lvl] = new_node;
+            }
+        }
+
+        m_size++;
+        return true;
+    }
 
     /**
      * @brief Removes an element with @p data from the list.
@@ -82,14 +181,45 @@ public:
      * @param data The data of the item to be removed from the list.
      * @return True if removed, otherwise false.
      */
-    bool remove(const T &data);
+    bool remove(const T &data)
+    {
+        Node *curr = m_head;
+        Node *node_to_rm = nullptr;
+
+        for (int lvl = MAX_LEVEL - 1; lvl >= 0; lvl--)
+        {
+            while (curr->levels[lvl] != nullptr &&
+                   curr->levels[lvl]->data < data)
+            {
+                curr = curr->levels[lvl];
+            }
+
+            if (curr->levels[lvl] != nullptr && curr->levels[lvl]->data == data)
+            {
+                node_to_rm = curr->levels[lvl];
+                curr->levels[lvl] = curr->levels[lvl]->levels[lvl];
+            }
+        }
+
+        if (node_to_rm != nullptr)
+        {
+            delete node_to_rm;
+            m_size--;
+            return true;
+        }
+
+        return false;
+    }
 
     /**
      * @brief Returns the number of nodes in the list.
      *
      * @return Number of nodes in the list.
      */
-    size_t size() const;
+    size_t size() const
+    {
+        return m_size;
+    }
 
     /**
      * @brief Checks if two skip lists are equal in values and order.
@@ -97,7 +227,25 @@ public:
      * @param other Reference to other skip list.
      * @return True, if values and order are identical. False otherwise.
      */
-    bool operator==(const SkipList &other) const;
+    bool operator==(const SkipList &other) const
+    {
+        if (this->m_size != other.m_size)
+        {
+            return false;
+        }
+
+        Node *self_curr = m_head->levels[0];
+        Node *other_curr = other.m_head->levels[0];
+        for (int i = 0; i < m_size; i++)
+        {
+            if (self_curr->data != other_curr->data)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
 
     /**
      * @brief Checks if two skip lists are inequal in values and order.
@@ -105,10 +253,29 @@ public:
      * @param other Reference to other skip list.
      * @return True, if values and order are not identical. False otherwise.
      */
-    bool operator!=(const SkipList &other) const;
+    bool operator!=(const SkipList &other) const
+    {
+        return not(*this == other);
+    }
 
 private:
     size_t m_size; ///< Number of nodes in the skip list.
     Node *m_head; ///< Pointer to the first node in the skip list.
+
+    void SkipListSetUp()
+    {
+        srand(time(0));
+        m_head = new Node(T(), MAX_LEVEL);
+
+        for (int i = 0; i < MAX_LEVEL; i++)
+        {
+            m_head->levels[i] = nullptr;
+        }
+    }
+
+    FRIEND_TEST(BasicSkipListTest, DefaultConstructor);
+    FRIEND_TEST(BasicSkipListTest, Insert);
+    FRIEND_TEST(BasicSkipListTest, Erase);
+    FRIEND_TEST(BasicSkipListTest, MoveConstructor);
 };
 }
--- homework/tests/tests_hw10.cpp
+++ homework/tests/tests_hw10.cpp
@@ -1,1 +1,86 @@
 #include "hw10.h"
+#include "gtest/gtest.h"
+#include "gtest/gtest_prod.h"
+#include <stdexcept>
+#include <iostream>
+
+namespace cppclass
+{
+class BasicSkipListTest : public ::testing::Test
+{
+protected:
+    BasicSkipListTest()
+    {
+    }
+
+    virtual ~BasicSkipListTest()
+    {
+    }
+
+    virtual void SetUp()
+    {
+    }
+
+    virtual void TearDown()
+    {
+    }
+};
+
+TEST_F(BasicSkipListTest, DefaultConstructor)
+{
+    SkipList<int, 20> sl;
+    EXPECT_EQ(sl.m_head->height, 20);
+    EXPECT_EQ(sl.m_size, 0);
+}
+
+TEST_F(BasicSkipListTest, Insert)
+{
+    SkipList<int, 20> sl;
+    sl.insert(10);
+    EXPECT_EQ(sl.size(), 1);
+
+    EXPECT_EQ(sl.m_head->levels[0]->data, 10);
+}
+
+TEST_F(BasicSkipListTest, Erase)
+{
+    SkipList<int, 20> sl;
+    sl.insert(10);
+    EXPECT_TRUE(sl.remove(10));
+
+    EXPECT_EQ(sl.size(), 0);
+}
+
+TEST_F(BasicSkipListTest, ArrayConstructor)
+{
+    int arr[20];
+    for (int i = 0; i < 20; i++)
+    {
+        *(arr + i) = i;
+    }
+
+    SkipList<int, 20> sl(arr, 20);
+    EXPECT_EQ(sl.size(), 20);
+    for (int i = 0; i < 20; i++)
+    {
+        EXPECT_NE(sl.search(i), nullptr);
+    }
+}
+
+TEST_F(BasicSkipListTest, CopyConstructor)
+{
+    int arr[20];
+    for (int i = 0; i < 20; i++)
+    {
+        *(arr + i) = i;
+    }
+
+    SkipList<int, 20> src(arr, 20);
+    SkipList<int, 20> sl(src);
+    EXPECT_TRUE(sl == src);
+}
+
+TEST_F(BasicSkipListTest, MoveConstructor)
+{
+}
+}
