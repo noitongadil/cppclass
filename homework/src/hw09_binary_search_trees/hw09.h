816d35f9e76bab15d88d772ebdfdd83381e85ec9
noitongadil <btrglm@proton.me>
2025-10-28 17:59:34 -0600

    hw09 methods implemented + tests

--- homework/src/hw09_binary_search_trees/hw09.h
+++ homework/src/hw09_binary_search_trees/hw09.h
@@ -1,105 +1,477 @@
 #pragma once
 
+#include <algorithm> // for std::sort
+#include <iomanip> // for std::setw
+#include <iostream> // for std::cout
+#include <vector> // for std::vector
 #include <cstddef> // for size_t
+#include "gtest/gtest_prod.h"
 
-namespace cppclass {
-
-template <typename T>
-class BinarySearchTree {
+namespace cppclass
+{
+template <typename T> class BinarySearchTree
+{
 public:
-    struct Node {
+    struct Node
+    {
         T data;
-        Node* left;
-        Node* right;
+        Node *right;
+        Node *left;
 
-        Node(T val) : data(val), left(nullptr), right(nullptr) {}
+        /**
+         * @brief Constructs an empty Node.
+         *
+         * @param val Data for the Node to hold.
+         */
+        Node(T val)
+            : data(val)
+            , left(nullptr)
+            , right(nullptr)
+        {
+        }
     };
 
-    /**
-    * @brief An empty BinarySearchTree will be created.
-    */
-    BinarySearchTree();
+    /// @brief An empty BinarySearchTree will be created.
+    BinarySearchTree()
+        : m_root(nullptr)
+        , m_size(0)
+    {
+    }
 
     /**
-    * @brief Constructor that initializes the tree with an array of values.
-    * @param arr Pointer to an array of values.
-    * @param size Size of the array.
-    */
-    BinarySearchTree(const T* arr, int size);
+     * @brief Constructor that initializes the tree with an array of values.
+     *
+     * @param arr Pointer to an array of values.
+     * @param size Size of the array.
+     */
+    BinarySearchTree(const T *arr, size_t size)
+        : m_root(nullptr)
+        , m_size(0)
+    {
+        std::vector<T> local_arr(arr, arr + size);
+        if (not std::is_sorted(local_arr.begin(), local_arr.end()))
+        {
+            std::sort(local_arr.begin(), local_arr.end());
+        }
+
+        bisection_insert(local_arr, 0, size);
+    }
 
     /**
-    * @brief Copy constructor for BinarySearchTree.
-    * @param other Reference to BinarySearchTree to copy from.
-    */
-    BinarySearchTree(const BinarySearchTree& other);
+     * @brief Copy constructor for BinarySearchTree.
+     *
+     * @param other Reference to BinarySearchTree to copy from.
+     */
+    BinarySearchTree(const BinarySearchTree &other)
+        : m_root(nullptr)
+        , m_size(0)
+    {
+        prefix_insert(other.m_root);
+    }
 
     /**
-    * @brief Move constructor for BinarySearchTree.
-    * @param other R-value reference to another BinarySearchTree object.
-    */
-    BinarySearchTree(BinarySearchTree&& other);
+     * @brief Move constructor for BinarySearchTree.
+     *
+     * @param other R-value reference to another BinarySearchTree object.
+     */
+    BinarySearchTree(BinarySearchTree &&other)
+        : m_root(other.m_root)
+        , m_size(other.m_size)
+    {
+        other.m_root = nullptr;
+        other.m_size = 0;
+    }
+
+    /// @brief Destructor for BinarySearchTree.
+    ~BinarySearchTree()
+    {
+        postfix_del(m_root);
+    }
 
     /**
-    * @brief Destructor for BinarySearchTree.
-    */
-    ~BinarySearchTree();
+     * @brief Inserts a value into the binary search tree.
+     *
+     * @param value The value to insert. Cannot be a duplicate.
+     * @return True if the value was inserted successfully, false if it already exists.
+     */
+    bool insert(const T &value)
+    {
+        auto current = m_root;
+
+        if (m_size == 0)
+        {
+            m_root = new Node(value);
+            m_size++;
+            return true;
+        }
+
+        while (current != nullptr)
+        {
+            if (value > current->data)
+            {
+                if (current->right == nullptr)
+                {
+                    current->right = new Node(value);
+                    m_size++;
+                    return true;
+                }
+                current = current->right;
+            }
+            else if (value < current->data)
+            {
+                if (current->left == nullptr)
+                {
+                    current->left = new Node(value);
+                    m_size++;
+                    return true;
+                }
+                current = current->left;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return false;
+    }
 
     /**
-    * @brief Inserts a value into the binary search tree.
-    * @param value The value to insert. Cannot be a duplicate.
-    * @return True if the value was inserted successfully, false if it already exists.
-    */
-    bool insert(T value);
+     * @brief Removes a value from the binary search tree.
+     *
+     * @param value The value to remove. Must be present in the tree.
+     * @return True if the value was removed successfully, false if it was not found.
+     */
+    bool remove(const T &value)
+    {
+        Node *target = m_root;
+        Node *parent = nullptr;
+
+        while (target != nullptr)
+        {
+            if (target->data > value)
+            {
+                parent = target;
+                target = target->left;
+            }
+            else if (target->data < value)
+            {
+                parent = target;
+                target = target->right;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        if (target == nullptr)
+        {
+            return false;
+        }
+
+        if (target->right != nullptr)
+        {
+            auto current = target->right;
+            if (current->left == nullptr)
+            {
+                target->data = current->data;
+                target->right = current->right;
+                delete current;
+                m_size--;
+                return true;
+            }
+
+            while (current->left->left != nullptr)
+            {
+                current = current->left;
+            }
+
+            target->data = current->left->data;
+            if (current->left->right != nullptr)
+            {
+                Node *tmp = current->left->right;
+                delete current->left;
+                m_size--;
+                current->left = tmp;
+            }
+            else
+            {
+                delete current->left;
+                current->left = nullptr;
+                m_size--;
+            }
+        }
+        else if (target->left != nullptr)
+        {
+            auto current = target->left;
+            if (current->right == nullptr)
+            {
+                target->data = current->data;
+                target->left = current->left;
+                delete current;
+                m_size--;
+                return true;
+            }
+
+            while (current->right->right != nullptr)
+            {
+                current = current->right;
+            }
+
+            target->data = current->right->data;
+            if (current->right->left != nullptr)
+            {
+                Node *tmp = current->right->left;
+                delete current->right;
+                m_size--;
+                current->right = tmp;
+            }
+            else
+            {
+                delete current->right;
+                current->right = nullptr;
+                m_size--;
+            }
+        }
+        else
+        {
+            if (parent == nullptr)
+            {
+                delete target;
+                m_root = nullptr;
+                m_size--;
+                return true;
+            }
+
+            if (parent->left == target)
+            {
+                parent->left = nullptr;
+            }
+            else
+            {
+                parent->right = nullptr;
+            }
+
+            delete target;
+            m_size--;
+        }
+
+        return true;
+    }
 
     /**
-    * @brief Removes a value from the binary search tree.
-    * @param value The value to remove. Must be present in the tree.
-    * @return True if the value was removed successfully, false if it was not found.
-    */
-    bool remove(T value);
+     * @brief Checks if a value is contained in the binary search tree.
+     *
+     * @param value The value to check.
+     * @return True if the value is found, false otherwise.
+     */
+    bool contains(T value) const
+    {
+        Node *current = m_root;
+        while (current)
+        {
+            if (current->data > value)
+            {
+                current = current->left;
+            }
+            else if (current->data < value)
+            {
+                current = current->right;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 
     /**
-    * @brief Checks if a value is contained in the binary search tree.
-    * @param value The value to check.
-    * @return True if the value is found, false otherwise.
-    */
-    bool contains(T value) const;
+     * @brief Returns the size of the binary search tree.
+     *
+     * @return The number of nodes in the tree.
+     */
+    size_t size() const
+    {
+        return m_size;
+    }
 
     /**
-    * @brief Returns the size of the binary search tree.
-    * @return The number of nodes in the tree.
-    */
-    size_t size() const;
+     * @brief Checks if two binary search trees are equal.
+     *
+     * @param other The other binary search tree to compare with.
+     * @return True if the trees are equal, false otherwise.
+     */
+    bool operator==(const BinarySearchTree &other) const
+    {
+        return prefix_comp(m_root, other.m_root);
+    }
 
     /**
-    * @brief Checks if two binary search trees are equal.
-    * @param other The other binary search tree to compare with.
-    * @return True if the trees are equal, false otherwise.
-    */
-    bool operator==(const BinarySearchTree& other) const;
-
-    /**
-    * @brief Checks if the binary search tree is not equal to another tree.
-    * @param other The other binary search tree to compare with.
-    * @return True if the trees are not equal, false otherwise.
-    */
-    bool operator!=(const BinarySearchTree& other) const;
+     * @brief Checks if the binary search tree is not equal to another tree.
+     *
+     * @param other The other binary search tree to compare with.
+     * @return True if the trees are not equal, false otherwise.
+     */
+    bool operator!=(const BinarySearchTree &other) const
+    {
+        return !(*this == other);
+    }
 
 private:
-    /**
-    * @brief Prints the binary search tree in-order.
-    */
-    void print() const;
+    /// @brief Prints the binary search tree in-order.
+    void print() const
+    {
+        print_helper(m_root, 0);
+    }
 
     /**
-    * @brief Checks if the binary search tree is valid.
-    * @return True if the tree is valid, false otherwise.
-    */
-    bool isValid() const;
+     * @brief Checks if the binary search tree is valid.
+     *
+     * @return True if the tree is valid, false otherwise.
+     */
+    bool isValid() const
+    {
+        return isValid_helper(m_root, nullptr, nullptr);
+    }
 
-    Node* m_root;
+    /**
+     * @brief Helper for validating BST subtree.
+     *
+     * @param node Pointer to node to validate.
+     * @param min Pointer to the lower bound.
+     * @param max Pointer to the upper bound.
+     * @return True if subtree is a valid BST within (min, max), false otherwise.
+     */
+    bool isValid_helper(Node *node, const T *min, const T *max) const
+    {
+        if (node == nullptr)
+        {
+            return true;
+        }
+
+        if ((min != nullptr && node->data <= *min) ||
+            (max != nullptr && node->data >= *max))
+        {
+            return false;
+        }
+
+        return isValid_helper(node->left, min, &(node->data)) &&
+               isValid_helper(node->right, &(node->data), max);
+    }
+
+    /**
+     * @brief Print helper that prints the nodes with padded spaces on the left.
+     *
+     * @param current Pointer to the node to start printing from.
+     * @param level Indentation level used to align output.
+     */
+    void print_helper(Node *current, size_t level) const
+    {
+        if (current == nullptr)
+        {
+            return;
+        }
+
+        print_helper(current->right, level + 2);
+        std::cout << std::setw(level * 2) << current->data << std::endl;
+        print_helper(current->left, level + 2);
+    }
+
+    /**
+     * @brief Delete all nodes in a tree using post-order traversal.
+     *
+     * @param current Pointer to the node to start deleting from. 
+     *                If nullptr, does nothing.
+     */
+    void postfix_del(Node *current)
+    {
+        if (current == nullptr)
+        {
+            return;
+        }
+
+        postfix_del(current->left);
+        postfix_del(current->right);
+        delete current;
+    }
+
+    /**
+     * @brief Insert nodes into this tree by traversing another tree in prefix order.
+     *
+     * @param current Pointer to a node in the other tree insert into this tree.
+     */
+    void prefix_insert(Node *current)
+    {
+        if (current == nullptr)
+        {
+            return;
+        }
+
+        this->insert(current->data);
+        prefix_insert(current->left);
+        prefix_insert(current->right);
+    }
+
+    /**
+     * @brief Build a balanced BST by inserting the middle elements of a sorted array.
+     *
+     * @param arr Sorted array of values.
+     * @param lower Lower bound.
+     * @param upper Upper bound.
+     */
+    void bisection_insert(std::vector<T> &arr, size_t lower, size_t upper)
+    {
+        if (lower >= upper)
+        {
+            return;
+        }
+
+        size_t midpoint = (lower + upper) / 2;
+        this->insert(arr[midpoint]);
+        bisection_insert(arr, lower, midpoint);
+        bisection_insert(arr, midpoint + 1, upper);
+    }
+
+    /**
+     * @brief Compare two trees for structural and value equality using prefix traversal.
+     *
+     * @param self_curr Root of the tree in this.
+     * @param other_curr Root of the tree in other.
+     * @return True if trees are identical in structure and values, false otherwise.
+     */
+    bool prefix_comp(Node *self_curr, Node *other_curr) const
+    {
+        if (self_curr == nullptr && other_curr == nullptr)
+        {
+            return true;
+        }
+
+        if (self_curr == nullptr || other_curr == nullptr)
+        {
+            return false;
+        }
+
+        if (self_curr->data != other_curr->data)
+        {
+            return false;
+        }
+
+        return prefix_comp(self_curr->left, other_curr->left) &&
+               prefix_comp(self_curr->right, other_curr->right);
+    }
+
+    Node *m_root;
     size_t m_size;
+
+    FRIEND_TEST(BasicBinarySearchTreeTest, DefaultConstructor);
+    FRIEND_TEST(BasicBinarySearchTreeTest, GetSizeWithMutating);
+    FRIEND_TEST(BasicBinarySearchTreeTest, MoveConstructor);
+    FRIEND_TEST(BasicBinarySearchTreeTest, CopyConstructor);
+    FRIEND_TEST(BasicBinarySearchTreeTest, ArrayConstructor);
+    FRIEND_TEST(BasicBinarySearchTreeTest, TreeValidation);
+    FRIEND_TEST(BasicBinarySearchTreeTest, Remove);
+    FRIEND_TEST(BinarySearchTreeTest, ACID);
 };
-
 } // namespace cppclass
-
--- homework/tests/tests_hw09.cpp
+++ homework/tests/tests_hw09.cpp
@@ -1,1 +1,303 @@
-#include <hw09.h>
+#include <cstdlib>
+
+#include "hw09.h"
+#include "gtest/gtest.h"
+#include "gtest/gtest_prod.h"
+
+namespace cppclass
+{
+class BasicBinarySearchTreeTest : public ::testing::Test
+{
+protected:
+    BasicBinarySearchTreeTest()
+    {
+    }
+
+    virtual ~BasicBinarySearchTreeTest()
+    {
+    }
+
+    virtual void SetUp()
+    {
+        auto seed_gen = testing::UnitTest::GetInstance();
+        int seed = seed_gen->random_seed();
+        std::srand(seed);
+        printf("seed: %i\n", seed);
+    }
+
+    virtual void TearDown()
+    {
+    }
+};
+
+TEST_F(BasicBinarySearchTreeTest, DefaultConstructor)
+{
+    BinarySearchTree<int> bst;
+    EXPECT_EQ(bst.m_root, nullptr);
+    EXPECT_EQ(bst.m_size, 0);
+}
+
+TEST_F(BasicBinarySearchTreeTest, GetSizeWithMutating)
+{
+    BinarySearchTree<int> bst;
+    EXPECT_EQ(bst.size(), 0);
+
+    bst.m_size = 200;
+    EXPECT_EQ(bst.size(), 200);
+
+    bst.m_size = 0;
+    EXPECT_EQ(bst.size(), 0);
+}
+
+TEST_F(BasicBinarySearchTreeTest, Insert)
+{
+    BinarySearchTree<int> bst;
+    EXPECT_TRUE(bst.insert(1));
+    EXPECT_TRUE(bst.insert(2));
+    EXPECT_TRUE(bst.insert(3));
+    EXPECT_TRUE(bst.insert(4));
+    EXPECT_TRUE(bst.insert(5));
+
+    // inserting duplicates
+    EXPECT_FALSE(bst.insert(4));
+    EXPECT_FALSE(bst.insert(5));
+}
+
+TEST_F(BasicBinarySearchTreeTest, TreeValidation)
+{
+    int arr[] = { 2, 3, 4, 5, 6, 7, 8 };
+    BinarySearchTree<int> bst(arr, sizeof(arr) / sizeof(*arr));
+
+    EXPECT_TRUE(bst.isValid());
+    ASSERT_EQ(bst.size(), 7);
+
+    bst.m_root->left->right->data = 9;
+    EXPECT_FALSE(bst.isValid());
+
+    bst.m_root->left->right->data = 4;
+    EXPECT_TRUE(bst.isValid());
+
+    bst.m_root->right->left->data = 4;
+    EXPECT_FALSE(bst.isValid());
+
+    bst.m_root->right->left->data = 6;
+    EXPECT_TRUE(bst.isValid());
+
+    BinarySearchTree<int> empty_bst;
+    EXPECT_TRUE(empty_bst.size() == 0);
+    EXPECT_TRUE(empty_bst.isValid());
+}
+
+TEST_F(BasicBinarySearchTreeTest, Contains)
+{
+    BinarySearchTree<int> bst;
+    for (int i = 0; i < 5; i++)
+    {
+        bst.insert(i + 1);
+    }
+
+    EXPECT_TRUE(bst.contains(5));
+    EXPECT_TRUE(bst.contains(4));
+    EXPECT_TRUE(bst.contains(3));
+    EXPECT_TRUE(bst.contains(2));
+    EXPECT_TRUE(bst.contains(1));
+
+    EXPECT_FALSE(bst.contains(0));
+    EXPECT_FALSE(bst.contains(6));
+}
+
+TEST_F(BasicBinarySearchTreeTest, ArrayConstructor)
+{
+    int arr[] = { 1, 2, 3, 4, 5 };
+    BinarySearchTree<int> bst(arr, sizeof(arr) / sizeof(*arr));
+    EXPECT_EQ(bst.size(), 5);
+    EXPECT_EQ(bst.m_root->data, 3);
+
+    EXPECT_TRUE(bst.contains(1));
+    EXPECT_TRUE(bst.contains(2));
+    EXPECT_TRUE(bst.contains(3));
+    EXPECT_TRUE(bst.contains(4));
+    EXPECT_TRUE(bst.contains(5));
+    EXPECT_TRUE(bst.isValid());
+}
+
+TEST_F(BasicBinarySearchTreeTest, CopyConstructor)
+{
+    int arr[] = { 1, 2, 3, 4, 5 };
+    BinarySearchTree<int> src(arr, sizeof(arr) / sizeof(*arr));
+
+    BinarySearchTree<int> bst(src);
+    EXPECT_EQ(bst.size(), src.size());
+    EXPECT_TRUE(bst.m_root->data == src.m_root->data);
+    EXPECT_TRUE(bst.isValid());
+    EXPECT_TRUE(src.isValid());
+}
+
+TEST_F(BasicBinarySearchTreeTest, MoveConstructor)
+{
+    int arr[] = { 1, 2, 3, 4, 5 };
+    BinarySearchTree<int> src(arr, sizeof(arr) / sizeof(*arr));
+    EXPECT_EQ(src.size(), 5);
+    EXPECT_EQ(src.m_root->data, 3);
+    EXPECT_TRUE(src.isValid());
+    auto p_src_root = src.m_root;
+
+    BinarySearchTree<int> bst(std::move(src));
+    EXPECT_EQ(bst.size(), 5);
+    EXPECT_EQ(bst.m_root->data, 3);
+    EXPECT_EQ(src.size(), 0);
+    EXPECT_EQ(src.m_root, nullptr);
+    EXPECT_EQ(p_src_root, bst.m_root);
+    EXPECT_TRUE(bst.isValid());
+}
+
+TEST_F(BasicBinarySearchTreeTest, Remove)
+{
+    int arr[] = { 5, 3, 7, 2, 4, 6, 8 };
+    BinarySearchTree<int> bst(arr, sizeof(arr) / sizeof(*arr));
+    EXPECT_EQ(bst.size(), 7);
+
+    // leaf node deletion
+    EXPECT_TRUE(bst.remove(2));
+    EXPECT_EQ(bst.size(), 6);
+    EXPECT_FALSE(bst.contains(2));
+    EXPECT_TRUE(bst.isValid());
+
+    // one child
+    EXPECT_TRUE(bst.remove(3));
+    EXPECT_EQ(bst.size(), 5);
+    EXPECT_FALSE(bst.contains(3));
+    EXPECT_TRUE(bst.isValid());
+
+    // two children
+    EXPECT_TRUE(bst.remove(5));
+    EXPECT_EQ(bst.size(), 4);
+    EXPECT_FALSE(bst.contains(5));
+    EXPECT_TRUE(bst.isValid());
+
+    EXPECT_FALSE(bst.remove(10));
+    EXPECT_EQ(bst.size(), 4);
+    EXPECT_TRUE(bst.isValid());
+
+    BinarySearchTree<int> empty_bst;
+    EXPECT_FALSE(empty_bst.remove(123));
+}
+
+class BinarySearchTreeTest : public BasicBinarySearchTreeTest
+{
+protected:
+    BinarySearchTree<int> *p_bst;
+
+    BinarySearchTreeTest()
+    {
+    }
+
+    virtual ~BinarySearchTreeTest()
+    {
+    }
+
+    virtual void SetUp()
+    {
+        BasicBinarySearchTreeTest::SetUp();
+        std::srand(std::time(0));
+
+        int arr[255];
+        for (int i = 0; i < 255; i++)
+        {
+            arr[i] = std::rand() % 255;
+        }
+
+        p_bst = new BinarySearchTree<int>(arr, sizeof(arr) / sizeof(*arr));
+    }
+
+    virtual void TearDown()
+    {
+        BasicBinarySearchTreeTest::TearDown();
+
+        delete p_bst;
+    }
+};
+
+TEST_F(BinarySearchTreeTest, ACID)
+{
+    while (p_bst->size() != 0)
+    {
+        p_bst->remove(std::rand() % 255);
+        EXPECT_TRUE(p_bst->isValid());
+    }
+
+    for (int i = 0; i < 255; i++)
+    {
+        p_bst->insert(std::rand() % 255);
+        EXPECT_TRUE(p_bst->isValid());
+    }
+
+    while (p_bst->size() != 0)
+    {
+        p_bst->remove(std::rand() % 255);
+        EXPECT_TRUE(p_bst->isValid());
+    }
+
+    for (int i = 0; i < 255; i++)
+    {
+        p_bst->insert(std::rand() % 255);
+        EXPECT_TRUE(p_bst->isValid());
+    }
+}
+
+class MultiBinarySearchTreeTest : public BasicBinarySearchTreeTest
+{
+protected:
+    BinarySearchTree<int> *p_bst_first;
+    BinarySearchTree<int> *p_bst_second;
+    BinarySearchTree<int> *p_bst_first_copy;
+
+    MultiBinarySearchTreeTest()
+    {
+    }
+
+    virtual ~MultiBinarySearchTreeTest()
+    {
+    }
+
+    virtual void SetUp()
+    {
+        int first_arr[] = { 5, 3, 7, 2, 4, 6, 8 };
+        int second_arr[] = { 1, 2, 3, 4, 5, 6, 7 };
+
+        p_bst_first = new BinarySearchTree<int>(
+            first_arr, sizeof(first_arr) / sizeof(*first_arr));
+
+        p_bst_first_copy = new BinarySearchTree<int>(*p_bst_first);
+
+        p_bst_second = new BinarySearchTree<int>(
+            second_arr, sizeof(second_arr) / sizeof(*second_arr));
+    }
+
+    virtual void TearDown()
+    {
+        BasicBinarySearchTreeTest::TearDown();
+
+        delete p_bst_first;
+        delete p_bst_second;
+        delete p_bst_first_copy;
+    }
+};
+
+TEST_F(MultiBinarySearchTreeTest, SetUp)
+{
+    EXPECT_EQ(p_bst_first->size(), 7);
+    EXPECT_EQ(p_bst_second->size(), 7);
+}
+
+TEST_F(MultiBinarySearchTreeTest, NotEqual)
+{
+    EXPECT_TRUE(*p_bst_first != *p_bst_second);
+}
+
+TEST_F(MultiBinarySearchTreeTest, Equal)
+{
+    EXPECT_TRUE(*p_bst_first == *p_bst_first_copy);
+    EXPECT_FALSE(*p_bst_first == *p_bst_second);
+}
+}
